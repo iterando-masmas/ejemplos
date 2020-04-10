0e3c7dc8910fb260631aec08956e38e51ad961ca
iterando++ <62533056+iterando-masmas@users.noreply.github.com>
2020-04-09 22:28:27 -0500

    actualización de URL del artículo

--- Calculadora de complejos/Calculadora_Complejos.java
+++ Calculadora de complejos/Calculadora_Complejos.java
@@ -1,6 +1,6 @@
 /* Creado por Andrés Melgar
  * para iterando++ http://iterando.online/
- * entrada: 
+ * entrada: http://iterando.online/ejercicios-resueltos/calculadora-de-numeros-complejos-en-java/
  */
 package calculadora_complejos;
 
