0f0a3ba3972377e7a4724fbd85e4b350c0a60a55
iterando++ <62533056+iterando-masmas@users.noreply.github.com>
2020-04-09 22:21:18 -0500

    actualización de URL del artículo

--- Reconocimiento de nota LA/reconocimiento_nota_LA.py
+++ Reconocimiento de nota LA/reconocimiento_nota_LA.py
@@ -1,3 +1,6 @@
+#Creado por Andrés Melgar
+#para iterando++ http://iterando.online/
+#entrada: http://iterando.online/ejercicios-resueltos/reconocimiento-de-notas-musicales-en-python/
 frecuencia=int(input("Ingrese frecuencia (Hz): "))
 
 if frecuencia == 440:
