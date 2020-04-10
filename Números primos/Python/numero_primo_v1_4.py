6e73037cc0c25b97fcbc3a69cb149703461ab79f
iterando++ <62533056+iterando-masmas@users.noreply.github.com>
2020-04-09 22:47:08 -0500

    actualización de URL del artículo

--- Números primos/Python/numero_primo_v1_4.py
+++ Números primos/Python/numero_primo_v1_4.py
@@ -1,3 +1,6 @@
+#Creado por Andrés Melgar
+#para iterando++ http://iterando.online/
+#entrada: http://iterando.online/ejercicios-resueltos/verificacion-de-numeros-primos-en-python/
 import math
 
 n=int(input("Ingrese un número natural (>0): "))
