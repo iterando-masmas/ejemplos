9a7bf8235026589083cce413dba97cda4dd845d0
iterando++ <62533056+iterando-masmas@users.noreply.github.com>
2020-04-09 22:44:41 -0500

    actualización de URL del artículo

--- Números primos/Java/Numero_Primo_v1_3.java
+++ Números primos/Java/Numero_Primo_v1_3.java
@@ -1,3 +1,7 @@
+/* Creado por Andrés Melgar
+ * para iterando++ http://iterando.online/
+ * entrada: http://iterando.online/ejercicios-resueltos/verificacion-de-numeros-primos-en-java/
+ */
 package numero_primo_v1_3;
 
 import java.util.Scanner;
