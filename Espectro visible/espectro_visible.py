4804d0e94b1e1c79eef01511732c459a33e09d83
iterando++ <62533056+iterando-masmas@users.noreply.github.com>
2020-04-09 22:24:10 -0500

    actualización de URL del artículo

--- Espectro visible/espectro_visible.py
+++ Espectro visible/espectro_visible.py
@@ -1,3 +1,6 @@
+#Creado por Andrés Melgar
+#para iterando++ http://iterando.online/
+#entrada: http://iterando.online/ejercicios-resueltos/validacion-de-rango-del-espectro-de-luz-visible-en-python/
 lon_onda=float(input("Ingrese longitud de onda (nm): "))
          
 if lon_onda >= 380 and lon_onda <= 780:
