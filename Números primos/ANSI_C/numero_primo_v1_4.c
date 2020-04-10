59b8e29eadfbbf04acfad82c6cd442da1d08053a
iterando++ <62533056+iterando-masmas@users.noreply.github.com>
2020-04-09 22:42:36 -0500

    actualización de URL del artículo

--- Números primos/ANSI_C/numero_primo_v1_4.c
+++ Números primos/ANSI_C/numero_primo_v1_4.c
@@ -1,1 +1,33 @@
+/* Creado por Andrés Melgar
+ * para iterando++ http://iterando.online/
+ * entrada: http://iterando.online/ejercicios-resueltos/verificacion-de-numeros-primos-en-c/
+ */
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    int n;
+    printf("Ingrese un número natural (>0): ");
+    scanf("%d", &n);
+
+    if (n <= 0)
+        printf("El número debe ser mayor que cero\n");
+    else {
+        int cant_divisores = 0;
+        int encontro_divisores = 0;
+        int limite = sqrt(n);
+        int i = 2;
+        while (i <= limite && !encontro_divisores) {
+            if (n % i == 0) {
+                cant_divisores++;
+                encontro_divisores = 1;
+            }
+            i++;
+        }
+        if (cant_divisores == 0 & n > 1)
+            printf("El número es primo\n");
+        else
+            printf("El número no es primo\n");
+    }
+    return 0;
+}
