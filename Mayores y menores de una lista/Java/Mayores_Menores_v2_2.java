69fca32bfebce6c6af77ccb1085e597f00bc4f59
iterando-masmas <Madau26>
2020-04-11 13:06:37 -0500

    corrección de identación

--- Mayores y menores de una lista/Java/Mayores_Menores_v2_2.java
+++ Mayores y menores de una lista/Java/Mayores_Menores_v2_2.java
@@ -10,39 +10,45 @@ public class Mayores_Menores_v2_2 {
 
     public static void main(String[] args) {
         int n, i = 0, mayor_magnitud1, mayor_magnitud2, menor_magnitud1, menor_magnitud2;
-    mayor_magnitud1 = mayor_magnitud2 = Integer.MIN_VALUE;
-    menor_magnitud1 = menor_magnitud2 = Integer.MAX_VALUE;
-    Scanner reader = new Scanner(System.in);
-    do {
-        System.out.printf("Ingrese número %d: ", i + 1);
-        n = reader.nextInt();
+        mayor_magnitud1 = mayor_magnitud2 = Integer.MIN_VALUE;
+        menor_magnitud1 = menor_magnitud2 = Integer.MAX_VALUE;
+        Scanner reader = new Scanner(System.in);
+        do {
+            System.out.printf("Ingrese número %d: ", i + 1);
+            n = reader.nextInt();
 
-        if (n < 0)
-            break;
-        i++;
-        if (n > mayor_magnitud1 || n > mayor_magnitud2)
-            if (n > mayor_magnitud1) {
-                mayor_magnitud2 = mayor_magnitud1;
-                mayor_magnitud1 = n;
-            } else
-                mayor_magnitud2 = n;
-        if (n < menor_magnitud1 || n < menor_magnitud2)
-            if (n < menor_magnitud1) {
-                menor_magnitud2 = menor_magnitud1;
-                menor_magnitud1 = n;
-            } else
-                menor_magnitud2 = n;
-    } while (true);
-    if (i == 0)
-        System.out.printf("No se ha ingresado ningún número%n");
-    else {
-        System.out.printf("El 1er número con mayor magnitud es: %d%n", mayor_magnitud1);
-        if (i > 1)
-            System.out.printf("El 2do número con mayor magnitud es: %d%n", mayor_magnitud2);
-        System.out.printf("El 1er número con menor magnitud es: %d%n", menor_magnitud1);
-        if (i > 1)
-            System.out.printf("El 2do número con menor magnitud es: %d%n", menor_magnitud2);
+            if (n < 0) {
+                break;
+            }
+            i++;
+            if (n > mayor_magnitud1 || n > mayor_magnitud2) {
+                if (n > mayor_magnitud1) {
+                    mayor_magnitud2 = mayor_magnitud1;
+                    mayor_magnitud1 = n;
+                } else {
+                    mayor_magnitud2 = n;
+                }
+            }
+            if (n < menor_magnitud1 || n < menor_magnitud2) {
+                if (n < menor_magnitud1) {
+                    menor_magnitud2 = menor_magnitud1;
+                    menor_magnitud1 = n;
+                } else {
+                    menor_magnitud2 = n;
+                }
+            }
+        } while (true);
+        if (i == 0) {
+            System.out.printf("No se ha ingresado ningún número%n");
+        } else {
+            System.out.printf("El 1er número con mayor magnitud es: %d%n", mayor_magnitud1);
+            if (i > 1) {
+                System.out.printf("El 2do número con mayor magnitud es: %d%n", mayor_magnitud2);
+            }
+            System.out.printf("El 1er número con menor magnitud es: %d%n", menor_magnitud1);
+            if (i > 1) {
+                System.out.printf("El 2do número con menor magnitud es: %d%n", menor_magnitud2);
+            }
+        }
     }
-    }
-    
 }
